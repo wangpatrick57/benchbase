10503e0d406c63d86bf9190a091f28b616524ba8
Patrick Wang <wang.patrick57@gmail.com>
2023-11-04 18:52:55 +0000

    tpcc replay run works on d7

--- tpcc_replay_run.sh
+++ tpcc_replay_run.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-DBLAB_DPATH="$HOME/Documents/mastersStuff/dblab"
-DBLAB_POSTGRES_DPATH="$DBLAB_DPATH/postgres"
+OS_NAME=$(uname -s)
+if [ "$OS_NAME" == "Linux" ]; then
+  DBLAB_POSTGRES_DPATH="$HOME/dblab/d7pg"
+elif [ "$OS_NAME" == "Darwin" ]; then
+  DBLAB_POSTGRES_DPATH="$HOME/Documents/mastersStuff/dblab/m1pg"
+else
+  echo "OS_NAME $OS_NAME is unknown"
+  exit 1
+fi
 GEN_LOG_CSV_SPATH="$DBLAB_POSTGRES_DPATH/postgresql.csv"
 PERMANENT_LOG_CSV_FPATH="$DBLAB_POSTGRES_DPATH/tpcc_log.csv"
 
 SKIP_BUILD=""
 
 # generate log file
-if false; then
+if true; then
   # this if block creates tpcc_log.csv
   SKIP_BUILD=$SKIP_BUILD ./run.sh tpcc-c-l
   SKIP_BUILD="1"
@@ -19,12 +26,12 @@ fi
 # [Invariant] tpcc_log.csv will have the correct values
 
 # reset DB
-if true; then
+if false; then
   SKIP_BUILD=$SKIP_BUILD ./run.sh tpcc-c-l # first, reset the database
   SKIP_BUILD="1" # if it was already 1, this is harmless
 fi
 
-if true; then
+if false; then
   # clean log file before running the replay so that we can track that as well
   rm "$(readlink $GEN_LOG_CSV_SPATH)"
 
